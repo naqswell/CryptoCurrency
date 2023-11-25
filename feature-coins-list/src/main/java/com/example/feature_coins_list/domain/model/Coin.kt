44615629eac760380c678ef94b831df40a3afa25
Nikita <naqswell@gmail.com>
2023-11-25 21:23:25 +0300

    Added domain Coin model

---
+++ feature-coins-list/src/main/java/com/example/feature_coins_list/domain/model/Coin.kt
@@ -0,0 +1,24 @@
+package com.example.feature_coins_list.domain.model
+
+data class Coin(
+    val availableSupply: Double,
+    val contractAddress: String,
+    val decimals: Int,
+    val exp: List<String>,
+    val icon: String,
+    val id: String,
+    val marketCap: Double,
+    val name: String,
+    val price: Double,
+    val priceBtc: Double,
+    val priceChanged1d: Double,
+    val priceChanged1h: Double,
+    val priceChanged1w: Double,
+    val rank: Int,
+    val redditUrl: String,
+    val symbol: String,
+    val totalSupply: Double,
+    val twitterUrl: String,
+    val volume: Double,
+    val websiteUrl: String
+)
