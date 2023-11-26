0bab9c9b29c7ee5d79cac98900e59e7638d718e5
Nikita <naqswell@gmail.com>
2023-11-26 13:40:42 +0300

    Added coin details route implementation

--- feature-coin-details/src/main/java/com/example/feature_coin_details/presentation/CoinDetailsRoute.kt
+++ feature-coin-details/src/main/java/com/example/feature_coin_details/presentation/CoinDetailsRoute.kt
@@ -1,10 +1,39 @@
 package com.example.feature_coin_details.presentation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.feature_coin_details.presentation.components.CoinDetailsContent
+import com.example.feature_coin_details.presentation.components.TopBarCoinDetail
 
 @Composable
 internal fun CoinDetailsRoute(
     viewModel: CoinDetailViewModel = hiltViewModel()
 ) {
+    val coinState = viewModel.uiState.collectAsState()
+    val coin = coinState.value.coin
+    val isTracking = coinState.value.isTracking
+
+    Scaffold(
+        topBar = {
+            TopBarCoinDetail(
+                coinSymbol = coin?.symbol,
+                icon = coin?.icon,
+                onNavigateBack = { viewModel.onNavigateBack() },
+                isTracking = isTracking,
+                onClick = { coin?.let { } }
+            )
+        }
+    ) { innerPadding ->
+        coin?.let { coin ->
+            CoinDetailsContent(
+                coin = remember { coin },
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
 }
