58cfba5ef1c82f4f0f99155cb96231f930e13d17
Nikita <naqswell@gmail.com>
2023-11-26 15:06:01 +0300

    Added domain model

---
+++ feature-coins-tracking-list/src/main/java/com/example/feature_coins_tracking_list/domain/model/CoinTracking.kt
@@ -0,0 +1,17 @@
+package com.example.feature_coins_tracking_list.domain.model
+
+import com.example.cache_coins.database.DateTypeConverter
+import java.util.Date
+
+data class CoinTracking(
+    val coinId: String,
+    val name: String?,
+    val symbol: String?,
+    val icon: String?,
+    val price: Double?,
+    val rank: Int,
+    val priceChanged1d: Double?,
+    val priceChanged1h: Double?,
+    val priceChanged1w: Double?,
+    val lastUpdated: Date?
+)
